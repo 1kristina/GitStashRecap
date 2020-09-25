65ca2c77abcbb08c2906e85590544899ba235bf0
1kristina <kristina.karyakina@gmail.com>
2020-09-24 21:29:09 -0400

    FeatureA file completed

--- src/com/stash/FeatureA.java
+++ src/com/stash/FeatureA.java
@@ -1,9 +1,13 @@
-package com.stash;
+git package com.stash;
 
 public class FeatureA {
 	public static void main(String[] args) {
 		
-		System.out.println("Tommorrow never");
+		System.out.println("Tommorrow never dies");
+		
+		System.out.println("FeatureA file modified");
+		
+		System.out.println("FeatureA completed");
 	}
 
 }
